6656f78fad5a0dbd5d867e12158204c903f112a4
Armaan Agrawal <armaan@Armaans-MacBook-Air.local>
2023-05-26 09:41:56 +0530

    Created Netflix Clone Successfully: First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import requests from "./requests";
+import { useState } from "react";
 
 function App() {
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [rowId, setRowId] = useState();
+
+  const data = {
+    rowId: rowId,
+    setRowId: setRowId,
+    trailerUrl: trailerUrl,
+    setTrailerUrl: setTrailerUrl,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+        data={data}
+      />
+      <Row
+        title={"Trending Now"}
+        fetchURL={requests.fetchTrending}
+        data={data}
+      />
+      <Row title={"Top Rated"} fetchURL={requests.fetchTopRated} data={data} />
+      <Row
+        title={"Action Movies"}
+        fetchURL={requests.fetchActionMovies}
+        data={data}
+      />
+      <Row
+        title={"Comedy Movies"}
+        fetchURL={requests.fetchComedyMovies}
+        data={data}
+      />
+      <Row
+        title={"Horror Movies"}
+        fetchURL={requests.fetchHorrorMovies}
+        data={data}
+      />
+      <Row
+        title={"Romance Movies"}
+        fetchURL={requests.fetchRomanceMovies}
+        data={data}
+      />
+      <Row
+        title={"Documentaries"}
+        fetchURL={requests.fetchDocumentaries}
+        data={data}
+      />
     </div>
   );
 }
